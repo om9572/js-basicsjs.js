b3f7004447ecd054599104cb63a7138be5e23b62
Om singh <114213189+om9572@users.noreply.github.com>
2022-09-24 15:37:35 +0530

    Add files via upload

---
+++ js basicjs.js
@@ -0,0 +1,116 @@
+// Single line comment
+/*
+    multi 
+    line 
+    comment
+*/
+//var x;
+//x=3;
+// let x;
+// x="Dhananjay"
+
+// const x=4;
+// x++;
+// console.log('value of x : ',x,'& Data type of X :',typeof x);
+
+//conditional statment
+var a=10;
+// if(a==101){
+//     console.log('value of A : ',a);
+// }
+
+// if(a==101){
+//     console.log('value of A : ',a);
+// }else{
+//     console.log('value of A is not equal to 10 : ',a);
+// }
+
+// var totalMarks = 55;
+// if(totalMarks>=80){
+//     console.log('Grade : A');
+// }else if(totalMarks<80 && totalMarks>=70){
+//     console.log('Grade : B');
+// }else if(totalMarks<70 && totalMarks>=60){
+//     console.log('Grade : C');
+// }else{
+//     console.log('Grade : D');
+// }
+
+// var apti = 'pass';
+// var tech = 'pass';
+// var hr = 'fail';
+
+// if(apti == 'pass'){
+//     if(tech=='pass'){
+//         if(hr=='pass'){
+//             console.log('Selected...')
+//         }else{
+//             console.log('Rejected...')
+//         }
+//     }else{
+//         console.log('Rejected...')
+//     }
+// }else{
+//     console.log('Rejected...')
+// }
+
+//Equals in Java script.
+// = Assignment operator
+// == comapre two value only.
+// === comare two value and datatype.
+
+//console.log(10===parseInt('10'));
+
+//Statment 
+
+var a=10;
+if(a==101){
+    console.log('value of A : ',a);
+}
+
+//Expression
+x= 3*5;
+
+x = x + 5;
+x += 8;
+
+//Operator A/C to operand operator have 3 type
+
+//1. unary 
+// ++, -- , +, -
+//2. Binary
+// +,-,*,/,%,=
+//3. ternary 
+// ? : 
+
+var a=10;
+var b=20;
+a+b
+//Operator A/C to peration operator have 4 type
+//1. Logical, conditional, assignment, bitwise, relational,arithmetic
+// && , || , !, !=, ==, === , !==
+//2. 
+
+
+var n = 123456789123456789
+undefined
+n
+123456789123456780
+typeof n
+'number'
+n+5
+123456789123456780
+var n = 123456789123456789n
+undefined
+n
+123456789123456789n
+typeof n
+'bigint'
+n*123456789123456789
+VM770:1 Uncaught TypeError: Cannot mix BigInt and other types, use explicit conversions
+    at <anonymous>:1:2
+(anonymous) @ VM770:1
+n*123456789123456789n
+15241578780673678515622620750190521n
+prompt();
+'10'
